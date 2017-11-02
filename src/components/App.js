d11a2e33efad94308f2f0e34113cb42f844a8f8c
James Kim <james@firstbuild.com>
2017-11-02 13:38:53 -0400

    feat: Add connect function with mapStateToProps and mapDispatchToProps to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
-import { addRecipe } from '../actions'
+import { addRecipe, removeFromCalendar } from '../actions'
+import { connect } from 'react-redux'
+
 
 class App extends Component {
+  doThing = () => {
+    this.props.selectRecipe({})
+  }
   render() {
+    console.log('Props', this.props)
     return (
       <div>
         Hello World
@@ -11,4 +17,28 @@ class App extends Component {
   }
 }
 
-export default App
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals;
+      }, {})
+    }))
+  };
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
